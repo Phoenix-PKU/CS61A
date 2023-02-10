b88e760d2e14ec16b8225044eb1fb512cd444793
LEILI <2602122712@qq.com>
2023-02-10 22:15:39 +0800

    lab10, get to know the basic syntax of scheme.

--- labs/lab10/lab10.scm
+++ labs/lab10/lab10.scm
@@ -1,22 +1,26 @@
-;(define (over-or-under num1 num2) 
+; (define (over-or-under num1 num2) 
 ;    (if (< num1 num2) -1 (if (= num1 num2) 0 1)))
-(define (over-or-under num1 num2) 
-    (cond ((< num1 num2) -1)
-          ((> num1 num2)  1)
-          (else 0)))
+(define (over-or-under num1 num2)
+  (cond 
+    ((< num1 num2) -1)
+    ((> num1 num2) 1)
+    (else          0)))
 
-(define (make-adder num) 
-    (define (adder inc) (+ num inc)) adder)
+(define (make-adder num)
+  (define (adder inc) (+ num inc))
+  adder)
 
-(define (composed f g) 
-    (define (f-g x) (f (g x)))
-    f-g)
+(define (composed f g)
+  (define (f-g x) (f (g x)))
+  f-g)
 
-;(define lst 
+; (define lst 
 ;    (cons '(1) (cons 2 
 ;        (cons (cons 3 (cons 4 nil)) (cons 5 nil)))))
-
 (define lst (list '(1) 2 '(3 4) 5))
 
-(define (duplicate lst) (if (null? lst) nil
-    (cons (car lst) (cons (car lst)  (duplicate (cdr lst)))) ))
+(define (duplicate lst)
+  (if (null? lst)
+      nil
+      (cons (car lst)
+            (cons (car lst) (duplicate (cdr lst))))))
