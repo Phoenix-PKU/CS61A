1885af7c380ba8dbc18b5cea7dc121198581b884
LEILI <2602122712@qq.com>
2023-02-10 21:17:36 +0800

    Complete lab10, get to know the basic syntax of scheme.

--- labs/lab10/lab10.scm
+++ labs/lab10/lab10.scm
@@ -1,9 +1,22 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+;(define (over-or-under num1 num2) 
+;    (if (< num1 num2) -1 (if (= num1 num2) 0 1)))
+(define (over-or-under num1 num2) 
+    (cond ((< num1 num2) -1)
+          ((> num1 num2)  1)
+          (else 0)))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+    (define (adder inc) (+ num inc)) adder)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) 
+    (define (f-g x) (f (g x)))
+    f-g)
 
-(define lst 'YOUR-CODE-HERE)
+;(define lst 
+;    (cons '(1) (cons 2 
+;        (cons (cons 3 (cons 4 nil)) (cons 5 nil)))))
 
-(define (duplicate lst) 'YOUR-CODE-HERE)
+(define lst (list '(1) 2 '(3 4) 5))
+
+(define (duplicate lst) (if (null? lst) nil
+    (cons (car lst) (cons (car lst)  (duplicate (cdr lst)))) ))
