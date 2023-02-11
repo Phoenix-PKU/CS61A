6f790ac09e90b1b89a79545fa31b9364bf2ab513
LEILI <2602122712@qq.com>
2023-02-11 14:41:28 +0800

    Complete hw08, scheme list practice.

--- hws/hw08/hw08.scm
+++ hws/hw08/hw08.scm
@@ -1,8 +1,27 @@
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s) 
+  (if (null? s) nil 
+  (if (pred (car s)) (cons (car s) (my-filter pred (cdr s))) 
+    (my-filter pred (cdr s)) )))
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2) 
+  (cond ((and (null? lst1) (null? lst2)) nil)
+        ((null? lst1)   lst2)
+        ((null? lst2)   lst1)
+        (else   (cons (car lst1) (cons (car lst2) (interleave (cdr lst1) (cdr lst2)))))))
 
 (define (accumulate joiner start n term)
-  'YOUR-CODE-HERE)
+  (if (= n 0) start 
+  (accumulate joiner (joiner start (term n)) (- n 1)  term)))
 
-(define (no-repeats lst) 'YOUR-CODE-HERE)
+(define (in-list? elem s) 
+  (if (null? s) false 
+   (or (= elem (car s)) (in-list? elem (cdr s))) ) )
+
+(define (no-repeats lst) 
+  (define (no-repeats-helper cur_lst lst) 
+    (if (null? lst) cur_lst
+      (if (in-list? (car lst) cur_lst) (no-repeats-helper cur_lst (cdr lst))
+      (no-repeats-helper (append cur_lst (list (car lst))) (cdr lst)))
+    )
+  )
+  (no-repeats-helper nil lst) )
