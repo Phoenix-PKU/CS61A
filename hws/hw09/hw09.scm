6bd9296db4d85ce4e0e91349b7fc1a505f4a2fa6
LEILI <2602122712@qq.com>
2023-07-03 12:50:26 +0800

    Working on hw09, I cannot solve switch and when macro.

--- hws/hw09/hw09.scm
+++ hws/hw09/hw09.scm
@@ -1,8 +1,21 @@
+(define (when condition exprs)
+  `(if ,condition (begin ,@exprs) 'okay))
+
+(when '(= 1 1) '((print 6) (print 1) 'a))
+
 (define-macro (when condition exprs)
-  'YOUR-CODE-HERE)
+  `(if ,condition (begin ,@exprs) 'okay))
+
+(define (switch expr cases)
+  (cons 'cond
+        (map (lambda (case)
+               (cons `(= ,expr ,(car case))
+                     (cdr case)))
+             cases)))
 
 (define-macro (switch expr cases)
-  (cons _________
-        (map (_________ (_________)
-                        (cons _________ (cdr case)))
+  (cons 'cond
+        (map (lambda (case)
+               (cons `(= ,expr ,(car case))
+                     (cdr case)))
              cases)))
